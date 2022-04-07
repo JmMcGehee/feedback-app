cc28fc0f6023698ec27ed73c80a2803b857cc8ae
Joshua McGehee <mcgehee.joshua@gmail.com>
2022-04-06 18:43:35 -0600

    Added ability to add a new feedback item after editing a feedback item

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -45,6 +45,12 @@ export const FeebackProvider = ({children}) => {
     setFeedback(
       feedback.map((item) => (item.id === id ? {...item, ...updItem } : item))
     )
+    setFeedbackEdit(
+      {
+        item: {},
+        edit: false
+      }
+    )
   }
 
   // Set item to be updated
